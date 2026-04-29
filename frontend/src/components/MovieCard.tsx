1828d1c65b46ccdeffafd22ffead2731060eb141
omERdem555 <omererdem555@outlook.com>
2026-04-30 01:10:26 +0300

    FAZ 6.1 — MovieCard.tsx (Presentation Layer)
    Bu adımda amaç:
    
    Veriyi göstermek, business logic eklemek değil.

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -5,23 +5,52 @@ type Props = {
 };
 
 export default function MovieCard({ movie }: Props) {
+  const shortOverview =
+    movie.overview.length > 100
+      ? movie.overview.slice(0, 100) + "..."
+      : movie.overview;
+
   return (
-    <div style={{ border: "1px solid #ddd", padding: 10 }}>
-      {movie.posterUrl && (
+    <div
+      style={{
+        border: "1px solid #ddd",
+        padding: 10,
+        borderRadius: "8px",
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+      }}
+    >
+      {movie.posterUrl ? (
         <img
           src={movie.posterUrl}
           alt={movie.title}
-          style={{ width: "100%" }}
+          style={{
+            width: "100%",
+            borderRadius: "4px",
+            marginBottom: "10px",
+          }}
         />
+      ) : (
+        <div
+          style={{
+            width: "100%",
+            height: "375px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#f0f0f0",
+            marginBottom: "10px",
+            borderRadius: "4px",
+          }}
+        >
+          No Image
+        </div>
       )}
 
       <h3>{movie.title}</h3>
 
-      <p>
-        {movie.overview.length > 100
-          ? movie.overview.slice(0, 100) + "..."
-          : movie.overview}
-      </p>
+      <p>{shortOverview}</p>
 
       <small>⭐ {movie.rating}</small>
     </div>
